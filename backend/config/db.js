949fbefc4f2ebef745057de837bd550fed079c08
Vinayak <mehtavinayak2004@gmail.com>
2025-10-25 16:17:22 +0530

    New Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
- export const connectDB=async() => {
-    await mongoose.connect('mongodb+srv://mehtavinayak2004:677204@cluster0.aqzej.mongodb.net/Food-Delivery').then(()=>console.log("DB Connected"));
-}
+export const connectDB = async () => {
+  try {
+    // Simplified MongoDB connection
+    await mongoose.connect('mongodb+srv://mehtavinayak2004:677204@cluster0.aqzej.mongodb.net/Food-Delivery');
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("Error connecting to DB:", error.message);
+  }
+};
+
--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
-import FoodItem from '../Fooditem/Fooditem'
+import FoodItem from '../FoodItem/FoodItem';
 const FoodDisplay = ({category}) => {
 
     const {food_list}=useContext(StoreContext)
